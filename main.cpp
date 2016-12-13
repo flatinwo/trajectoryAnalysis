d815fafb8a4d58d0b30b84eaab9befafe52693b8
Folarin <blfolarin@gmail.com>
2016-12-13 11:33:06 -0500

    mown main

--- main.cpp
+++ main.cpp
@@ -9,10 +9,15 @@
 
 #include <iostream>
 #include <cassert>
+#include <time.h>
 
 #include "trajectoryAnalysis.h"
 
 using namespace trajectoryAnalysis;
+
+void printTimeTaken(clock_t& tStart){
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -20,13 +25,40 @@ int main(int argc, const char * argv[]) {
 
     //const char* filename = "/Users/Folarin/Documents/Tests/gro_files/test_ql/em_conf_240.gro";
     //const char* filename = "/Users/Folarin/Documents/Tests/LJ/dump-npt.gro";
+   // const char* filename = "/Users/Folarin/Documents/Tests/xyz_files/em_conf.gro";
     
-    const char* filename = "/Users/Folarin/Documents/Tests/xyz_files/filenames";
+    //const char* filename = "/Users/Folarin/Documents/Tests/xyz_files/filenames";
     
-
+    const char* filename = "/Users/Folarin/Documents/Tests/xdr_files/otrj.xtc";
     
-    Trajectory traj = getCombinedTrajectory(filename);
+    //trajectory_t system;
+    //xdr_info info;
     
+    //loadxtc(filename, system, info);
+    
+    
+    //test(filename);
+    clock_t tStart = clock();
+    std::unique_ptr<Trajectory> traj;
+    
+    //traj.reset(new Trajectory(getCombinedTrajectory(filename)));
+    
+    traj.reset(new Trajectory(filename,FILETYPE::XTC,1,1,1000));
+    
+    traj->setComputeFskt(true,2.*M_PI/traj->getTrajectory()[0].box.box_period[0]);
+    traj->computeMeanSquaredDisplacement();
+    traj->setTimeStep(0.002); //ps
+    traj->printFskt();
+    
+    printTimeTaken(tStart);
+    
+   /* function1d_t gr = traj->computeGofR();//need to write copy
+    std::cout << gr;*/
+    
+    /*LSI lsi(*traj,2.3);
+    lsi.setRcutOff(3.5);
+    lsi.setBinSize(0.002);
+    lsi.print();*/
     
     
     /*traj.setComputeFskt(true,4.0);
@@ -43,19 +75,25 @@ int main(int argc, const char * argv[]) {
     bop.compute();
     bop.print();*/
     
-    TetrahedralOrderParameter analyzeq(traj);
-    analyzeq.setRcutOff(5.0);
-    analyzeq.setRmin(2.00);
     
-    /*analyzeq.setPositionTetrahedrality(true);
+    /*OrderParameter op(*traj);
+    op.setRcutOff(1.251);
+    //op.setRmin(0.99);
+    op.printNeighborDistribution();*/
+    
+    /*TetrahedralOrderParameter analyzeq(*traj);
+    analyzeq.setRcutOff(5.0);
+    //analyzeq.setRmin(2.00);
+    
+    analyzeq.setPositionTetrahedrality(true);
     //analyzeq.addRmax(1.25);
-    analyzeq.addRmax(1.50);
-    analyzeq.addRmax(1.75);
+    //analyzeq.addRmax(1.50);
+    //analyzeq.addRmax(1.75);
     analyzeq.addRmax(2.00);
-    analyzeq.addRmax(2.25);*/
+    //analyzeq.addRmax(2.25);
     
     analyzeq.compute();
-    analyzeq.print();
+    analyzeq.print();*/
     
     /*Trajectory Ensemble(filename,FILETYPE::GRO,1,4);
     AveragedBondOrderParameter BOP(Ensemble,6);
