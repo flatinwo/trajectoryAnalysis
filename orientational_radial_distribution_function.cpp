8a00b954729d6b0d0261ac2d0d368d6c8848ab9b
Folarin <blfolarin@gmail.com>
2017-03-14 18:18:56 -0400

    for file structure

--- orientational_radial_distribution_function.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  orientational_radial_distribution_function.cpp
-//  trajectoryAnalysis
-//
-//  Created by Folarin Latinwo on 6/21/16.
-//  Copyright © 2016 Folarin Latinwo. All rights reserved.
-//
-
-#include "orientational_radial_distribution_function.hpp"
--- structure_factor.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  structure_factor.cpp
-//  trajectoryAnalysis
-//
-//  Created by Folarin Latinwo on 6/15/16.
-//  Copyright © 2016 Folarin Latinwo. All rights reserved.
-//
-
-#include "structure_factor.hpp"
